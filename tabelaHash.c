0e136369eec4ae1af4db28a8e7ce5c3546a223ae
LuizFernand018 <luiz.00000859569@unicap.br>
2026-05-21 18:39:46 -0300

    adicionando funcao criaHash

--- tabelaHash.c
+++ tabelaHash.c
@@ -16,9 +16,9 @@ typedef struct tabelaHash{
 
 
 int valorString(TabelaHash th, char* senha);
-void criaHash();
+TabelaHash criaHash(int size);
 void insereHash();
-char buscaHash();
+char buscaHash(TabelaHash th, char* username, char* senha, int valor);
 char removeHash();
 void liberaHash();
 void imprimeHash();
@@ -40,16 +40,40 @@ int valorString(TabelaHash th, char* senha){
     return soma % th->size;
 }
 
-void criaHash(){
-    
+TabelaHash criaHash(int size){
+    TabelaHash th = malloc(sizeof(struct tabelaHash));
+
+    if(th != NULL){
+        th->size = size;
+        th->qtd = 0;
+        th->itens = malloc(sizeof(Usuario*) *size);
+        if(th->itens != NULL){
+            for(int i = 0; i < size; i++)
+                th->itens[i] = NULL;
+        }
+    }
+    return th;
 }
 
 void insereHash(){
     
 }
 
-char buscaHash(){
+char buscaHash(TabelaHash th, char* user, char* senha, int valor){
+    if(th->itens != NULL){
+        int indice = valorString(th, user);
+        int hashSenha = valorString(th, senha);
+        
+        Usuario *atual = th->itens[indice];
 
+        while(atual != NULL){
+            if(strcmp(atual->username, user) && atual->hashSenha == hashSenha)
+                return 0;
+        }
+        atual = atual->prox;
+    }
+
+    return 1;
 }
 
 char removeHash(){
